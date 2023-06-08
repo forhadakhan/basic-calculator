64efd428663c9ae89d1f3183edf9562804526499
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-08 23:46:38 +0600

    Updated design and defined all the handle function

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import BasicCalculator from './components/BasicCalculator'
 
 function App() {
   return (
-    <div className='bg-light bg-gradient min-height-full min-vh-100'>
-            <div className="container py-4 px-3 mx-auto">
-                <div className="d-flex align-items-center justify-content-center bg-white m-4 p-2 border rounded-3">
-                    <h1 className='display-6 bg-white'> <i class="bi bi-calculator-fill"></i> Basic Calculator </h1>
+    <div className='bg-info-subtle min-height-full min-vh-100'>
+            <div className="container px-3 mx-auto">
+                <div className="d-flex align-items-center justify-content-center bg-white mb-4 p-2 border border-top-0 rounded-bottom-5">
+                    <h1 className='h-6 bg-white'> <i class="bi bi-calculator-fill"></i> Basic Calculator </h1>
                 </div>
 
                 <BasicCalculator />
 
-                <div className="d-flex align-items-center justify-content-center bg-white mt-5 border rounded-3">
+                <div className="d-flex align-items-center justify-content-center bg-white mt-5 border border-bottom-0 rounded-top-5">
                     <p className='font-monospace pt-3 px-4 text-secondary'><a href='https://github.com/forhadakhan/' target='_blank'>@forhadakhan</a> coded it. You may find the code <a href='https://github.com/forhadakhan/basic-calculator' target='_blank'>repository here</a>.</p>
                 </div>
             </div>
--- src/components/BasicCalculator.jsx
+++ src/components/BasicCalculator.jsx
@@ -1,40 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 const BasicCalculator = () => {
-    // ...Calculator logic here...
+    const [displayValue, setDisplayValue] = useState('0');
+    const [cacheValue, setCacheValue] = useState('0');
+
+    const handleDigitLimit = (value, callback) => {
+        
+    };
+
+    const handleNumberClick = (value) => {
+        
+    };
+
+    const handleOperatorClick = (operator) => {
+        
+    };
+
+    const handleDecimalClick = () => {
+       
+    };
+
+    const handleDeleteClick = () => {
+       
+    };
+
+    const handleClearClick = () => {
+       
+    };
+
+    const handleEqualsClick = () => {
+        
+    };
+
+
+
 
     return (
-        <div id="basic-body" className=''>
-            <div className='container my-4 d-flex flex-column justify-content-center align-items-center'>
-                <div id="display" className='basic-display border rounded-3'></div>
+        <div id="basic-body" className="">
+            <div className="container my-4 d-flex flex-column justify-content-center align-items-center">
+                <div className='container mt-3 d-flex flex-column justify-content-center align-items-center'>
+                    <div id="display" className='basic-display border rounded-3'>
+                        <div className='text-end text-warning d-block formulaScreen'>{cacheValue}</div>
+                        <div className='text-end fs-4 p-1'>{displayValue}</div>
+                    </div>
+                </div>
             </div>
             <div className="container calculator border rounded-3">
-                <div className='my-1'>
-                    <button className="dr-key btn btn-secondary mx-1" id="clear" value="AC">AC</button>
-                    <button className="s-key btn btn-secondary m-1" id="divide" value="/">/</button>
-                    <button className="s-key btn btn-secondary m-1" id="multiply" value="x">x</button>
+                <div className="my-1">
+                    <button className="dr-key btn btn-warning mx-1  fs-4" id="clear" value="AC" onClick={handleClearClick}>AC</button>
+                    <button className="s-key btn btn-primary m-1  fs-4" id="divide" value="/" onClick={() => handleOperatorClick('/')}>/</button>
+                    <button className="s-key btn btn-primary m-1  fs-4" id="multiply" value="x" onClick={() => handleOperatorClick('*')}>x</button>
                 </div>
-                <div className='my-1'>
-                    <button className="s-key m-1 btn btn-secondary" id="seven" value="7">7</button>
-                    <button className="s-key m-1 btn btn-secondary" id="eight" value="8">8</button>
-                    <button className="s-key m-1 btn btn-secondary" id="nine" value="9">9</button>
-                    <button className="s-key m-1 btn btn-secondary" id="subtract" value="-">-</button>
+                <div className="my-1">
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="seven" value="7" onClick={() => handleNumberClick('7')}>7</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="eight" value="8" onClick={() => handleNumberClick('8')}>8</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="nine" value="9" onClick={() => handleNumberClick('9')}>9</button>
+                    <button className="s-key m-1 btn btn-primary  fs-4" id="subtract" value="-" onClick={() => handleOperatorClick('-')}>-</button>
                 </div>
-                <div className='my-1'>
-                    <button className="s-key m-1 btn btn-secondary" id="four" value="4">4</button>
-                    <button className="s-key m-1 btn btn-secondary" id="five" value="5">5</button>
-                    <button className="s-key m-1 btn btn-secondary" id="six" value="6">6</button>
-                    <button className="s-key m-1 btn btn-secondary" id="add" value="+">+</button>
+                <div className="my-1">
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="four" value="4" onClick={() => handleNumberClick('4')}>4</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="five" value="5" onClick={() => handleNumberClick('5')}>5</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="six" value="6" onClick={() => handleNumberClick('6')}>6</button>
+                    <button className="s-key m-1 btn btn-primary  fs-4" id="add" value="+" onClick={() => handleOperatorClick('+')}>+</button>
                 </div>
-                <div className='my-1'>
-                    <button className="s-key m-1 btn btn-secondary" id="one" value="1">1</button>
-                    <button className="s-key m-1 btn btn-secondary" id="two" value="2">2</button>
-                    <button className="s-key m-1 btn btn-secondary" id="three" value="3">3</button>
+                <div className="my-1">
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="one" value="1" onClick={() => handleNumberClick('1')}>1</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="two" value="2" onClick={() => handleNumberClick('2')}>2</button>
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="three" value="3" onClick={() => handleNumberClick('3')}>3</button>
                 </div>
-                <div className='my-1'>
-                    <button className="dr-key mx-1 btn btn-secondary" id="zero" value="0">0</button>
-                    <button className="s-key m-1 btn btn-secondary" id="decimal" value=".">.</button>
-                    <button className="dc-key m-1 btn btn-secondary" id="equals" value="=">=</button>
+                <div className="my-1">
+                    <button className="s-key m-1 btn btn-secondary  fs-4" id="decimal" value="." onClick={() => handleDecimalClick('.')}>.</button>
+                    <button className="s-key mx-1 btn btn-secondary  fs-4" id="zero" value="0" onClick={() => handleNumberClick('0')}>0</button>
+                    <button className="s-key mx-1 btn btn-danger  fs-4" id="delete" value="-1" onClick={() => handleDeleteClick('0')}>Del</button>
+                    <button className="dc-key m-1 btn btn-success  fs-4" id="equals" value="=" onClick={handleEqualsClick}>=</button>
                 </div>
             </div>
         </div>
